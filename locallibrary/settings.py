38d1b3445e2ebbe690ebf1f2f426f6b83b42103c
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-02-21 20:23:44 +0100

    Final

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -170,4 +170,4 @@ DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
-ALLOWED_HOSTS = 'https://locallibrary-myhq.onrender.com'
+ALLOWED_HOSTS += 'https://locallibrary-myhq.onrender.com'
